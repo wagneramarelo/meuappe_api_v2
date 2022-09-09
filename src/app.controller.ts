c8fa2f0ea9784f1e8b739f305675cd63f76a8719
wagner.amarelo <wamarelo1980@gmail.com>
2022-09-09 09:32:29 -0300

    inicial

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('objeto')
+  getObject(): string {
+    return this.appService.getObject();
+  }
 }
